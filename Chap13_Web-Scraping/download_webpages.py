98a6e6096d3c757279d6d0496430208b75d45ed6
Hans Lorico <hqlorico@up.edu.ph>
2025-09-22 23:09:09 +0800

    feat: handling errors download webpages

---
+++ Chap13_Web-Scraping/download_webpages.py
@@ -0,0 +1,15 @@
+import requests
+
+# response = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+# print(type(response))
+# print(response.status_code == requests.codes.ok)
+# print(len(response.text))
+# print(response.text[:210])
+
+# response = requests.get('https://inventwithpython.com/page_that_does_not_exist')
+# print(response.raise_for_status())
+
+# try:
+#     response.raise_for_status()
+# except Exception as exc:
+#     print(f'There was a problem: {exc}')
