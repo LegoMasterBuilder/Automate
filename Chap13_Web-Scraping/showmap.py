ce637261f7e818b49d377804958bbfea35a4271e
Hans Lorico <hqlorico@up.edu.ph>
2025-09-22 18:20:59 +0800

    feat: add step 2 from chap13

---
+++ Chap13_Web-Scraping/showmap.py
@@ -0,0 +1,10 @@
+# showmap.py - Launches a map in the browser using an address from the command line or clipboard
+
+import webbrowser, sys
+if len(sys.argv) > 1:
+    # Get address from command line.
+    address = ' '.join(sys.argv[1:])
+
+# TODO: Get address from clipboard.
+
+# TODO: Open the web browser.
