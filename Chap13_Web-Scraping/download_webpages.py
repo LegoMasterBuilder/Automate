6794607e8cc769b6be7d1f877cab2ccfad7e70e6
Hans Lorico <hqlorico@up.edu.ph>
2025-09-23 01:50:53 +0800

    feat: create weather_api.py

--- Chap13_Web-Scraping/download_webpages.py
+++ Chap13_Web-Scraping/download_webpages.py
@@ -13,3 +13,9 @@ import requests
 #     response.raise_for_status()
 # except Exception as exc:
 #     print(f'There was a problem: {exc}')
+
+response = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+response.raise_for_status()
+with open('RomeoAndJuliet.txt', 'wb') as play_file:
+    for chunk in response.iter_content(100000):
+        play_file.write(chunk)
