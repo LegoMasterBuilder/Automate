4ec8941c2e5fe95b7f82e5eeeb9dc7d8dbfb4f47
Hans Lorico <hqlorico@up.edu.ph>
2025-09-23 16:48:00 +0800

    feat: complete accessing a weather api section

--- Chap13_Web-Scraping/weather_api.py
+++ Chap13_Web-Scraping/weather_api.py
@@ -11,11 +11,27 @@ def get_current_weather():
     state_code = 'CA'
     country_code = 'US'
 
-    response = requests.get(f'https://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={os.getenv('api_key')}')
+    response = requests.get(f'https://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={os.getenv("api_key")}')
     print(response.text)
 
     response_data = json.loads(response.text)
     print(response.data)
+
+    lat = json.loads(response.text[0]['lat'])
+    lon = json.loads(response.text[0]['lon'])
+    response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}')
+    response_data = json.loads(response.text)
+    print(response_data)
+
+    kelvin = response_data['main']['temp']
+    print(kelvin)
+
+    # Convert Kelvin to Celsius
+    print(round(kelvin - 273.15, 1))
+
+    # Convert Kelvin to Fahrenheit
+    print(round(kelvin * (9 / 5) - 459.67, 1))
+    
 
 def main():
     configure()
