5c20a7091d564b0119718e386ae3d2b57a1fa85b
Hans Lorico <hqlorico@up.edu.ph>
2025-09-23 17:56:52 +0800

    feat: retrieve example3.html

---
+++ Chap13_Web-Scraping/parsing_html_w_bs4.py
@@ -0,0 +1,11 @@
+import requests, bs4
+
+res = requests.get('https://autbor.com/example3.html')
+res.raise_for_status()
+example_soup = bs4.BeautifulSoup(res.text, 'html.parser')
+print(type(example_soup))
+
+with open('example3.html') as example_file:
+    example_soup = bs4.BeautifulSoup(example_file, 'html.parser')
+
+print(type(example_soup))
