507c97f9a08ee5370f0ee392c2575c170ee9c0ad
Hans Lorico <hqlorico@up.edu.ph>
2025-09-23 18:08:18 +0800

    feat: get data from element attributes section

--- Chap13_Web-Scraping/parsing_html_w_bs4.py
+++ Chap13_Web-Scraping/parsing_html_w_bs4.py
@@ -9,3 +9,10 @@ with open('example3.html') as example_file:
     example_soup = bs4.BeautifulSoup(example_file, 'html.parser')
 
 print(type(example_soup))
+
+soup = bs4.BeautifulSoup(open('example3.html'), 'html.parser')
+span_elem = soup.select('span')[0]
+print(str(span_elem))
+print(span_elem.get('id'))
+print(span_elem.get('some_nonexistent_addr') == None)
+print(span_elem.attrs)
