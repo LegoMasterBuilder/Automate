e3c3eef62b5ff0fffcb185d8f0f6a1e4ba50814d
Hans Lorico <hqlorico@up.edu.ph>
2025-09-25 22:33:21 +0800

    feat: add multiplication table file

---
+++ Chap14_Excel-Spreadsheets/multiplicationTable.py
@@ -0,0 +1,7 @@
+# multiplicationTable.py
+
+# Create a program multiplicationTable.py that takes a number N 
+# from the command line and creates an N×N multiplication table 
+# in an Excel spreadsheet. For example, when the program is run 
+# like this: 
+# py multiplicationTable.py 6
