a77f6aea0d74500ef4739aefbabd2c8a7a42761f
Hans Lorico <hqlorico@up.edu.ph>
2025-09-27 01:46:03 +0800

    feat: add working blankRowInserter.py

--- Chap14_Excel-Spreadsheets/blankRowInserter.py
+++ Chap14_Excel-Spreadsheets/blankRowInserter.py
@@ -12,44 +12,43 @@ from openpyxl.utils import get_column_letter
 
 try:
     # Set row N and M blank rows.
-    # N, M = int(sys.argv[1]), int(sys.argv[2])
-    # spreadsheet = sys.argv[3]
-    N = 3
-    M = 2
-    spreadsheet = './Automate_the_Boring_Stuff_3e_onlinematerials/example3.xlsx'
+    N, M = int(sys.argv[1]), int(sys.argv[2])
+    spreadsheet = sys.argv[3]
+    # N = 3
+    # M = 2
+    # spreadsheet = './Automate_the_Boring_Stuff_3e_onlinematerials/example3.xlsx'
 
-    wb = openpyxl.load_workbook(spreadsheet)
-    sheet = wb.active
-    factor = 0
+    wb1 = openpyxl.load_workbook(spreadsheet)
+    sheetOrig = wb1['Sheet1']
+    wb2 = openpyxl.Workbook()
+    sheetNew = wb2['Sheet']
 
     for row in range(1, N):
-        for col in range(1, sheet.max_column + 1):
+        for col in range(1, sheetOrig.max_column + 1):
             column_letter = str(get_column_letter(col))
-            row_number = str(row + factor)
+            row_number = str(row)
             cell_loc = column_letter + row_number
             if row == N:
+                sheetNew[cell_loc] = " "
                 break
             else: 
-                print(row)
-                # print(sheet[cell_loc])
-                # sheet.cell(row=row, column=col).value = sheet[cell_loc]
+                sheetNew[cell_loc] = sheetOrig[cell_loc].value
 
-    for row in range(N+M, sheet.max_row + M+1):
-        for col in range(1, sheet.max_column + 1):
+    for row in range(N, sheetOrig.max_row + M+1):
+        for col in range(1, sheetOrig.max_column + 1):
             column_letter = str(get_column_letter(col))
-            row_number = str(row + factor)
-            cell_loc = column_letter + row_number
-            while M > 0:
-                M -= 1
-                print("blank")
-            print(row)
+            row_number_old = str(row)
+            row_number_new = str(row + M)
+            cell_loc_old = column_letter + row_number_old
+            cell_loc_new = column_letter + row_number_new
+            sheetNew[cell_loc_new] = sheetOrig[cell_loc_old].value
             
 
     # Save changes to a .xlsx file.
-    wb.save('blankRowInserted.xlsx')
+    wb2.save('editBlank.xlsx')
 
 except IndexError:
     print("Incomplete input. Please try again.")
 
-# except FileNotFoundError:
-#     print("File input not found. Please try again.")
+except FileNotFoundError:
+    print("File input not found. Please try again.")
